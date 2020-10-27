9dac606ccce8d5ec5ef29a2f8662ef86284808e8
Ronali <ronalipanigrahy88@gmail.com>
2020-10-27 22:22:37 +0530

    CODE TO CALCULATE PART-TIME WAGE ADDED

--- Employee.java
+++ Employee.java
@@ -7,17 +7,18 @@ public class Employee{
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH");
 		Employee e=new Employee();
 		e.check();
+		e.part();
 	}
 
 
-
+       	  Random r=new Random();
+          int random=r.nextInt(2);
+	  int part=r.nextInt(2);
 	public void check()
 	{
-		 Random r=new Random();
-                int random=r.nextInt(2);
 
 	 	if(random==1)
-                {
+	        {
                         System.out.println("EMPLOYEE IS PRESENT TODAY");
 			System.out.println("Wage/Hr=20 and Fulldayhours=8");
 			int wage=20*8;
@@ -28,8 +29,27 @@ public class Employee{
                         System.out.println("EMPLOYEE IS ABSENT TODAY");
                 }
 
-		
+
 	}
+
+	 public void part()
+        {
+
+                if(part==1)
+                {
+                        System.out.println("EMPLOYEE IS DOING PARTTIME TODAY");
+                        System.out.println("Wage/Hr=20 and Parttimehours=8");
+                        int wage=20*8;
+                        System.out.println("PARTTIMEWAGE WILL BE="+ wage);
+                }
+                else
+                {
+                        System.out.println("EMPLOYEE IS NOT DOING PART TIME TODAY");
+                }
+
+
+        }
+
 
 
 
