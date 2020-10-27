86995e09c75792f018c3a90f8ae83bdb7889d74b
Ronali <ronalipanigrahy88@gmail.com>
2020-10-27 22:00:41 +0530

    CODE TO CHECK ATTENDANCE OF EMPLOYEE ADDED

--- Employee.java
+++ Employee.java
@@ -1,7 +1,22 @@
+import java.util.*;
+
 public class Employee{
 
 	public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH");
+		Random r=new Random();
+		int random=r.nextInt(2);
+		System.out.println(random);
+
+		if(random==1)
+		{
+			System.out.println("EMPLOYEE IS PRESENT TODAY");
+		}
+		else
+		{
+			System.out.println("EMPLOYEE IS ABSENT TODAY");
+		}
+
 	}
 }
