a5cd88c3932580228421b1a1663e2b0a6c974d92
Ronali <ronalipanigrahy88@gmail.com>
2020-10-27 22:15:27 +0530

    Code for calculating dailywage added

--- Employee.java
+++ Employee.java
@@ -5,18 +5,32 @@ public class Employee{
 	public static void main(String[] args)
 	{
 		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM ON MASTER BRANCH");
-		Random r=new Random();
-		int random=r.nextInt(2);
-		System.out.println(random);
-
-		if(random==1)
-		{
-			System.out.println("EMPLOYEE IS PRESENT TODAY");
-		}
-		else
-		{
-			System.out.println("EMPLOYEE IS ABSENT TODAY");
-		}
-
+		Employee e=new Employee();
+		e.check();
 	}
+
+
+
+	public void check()
+	{
+		 Random r=new Random();
+                int random=r.nextInt(2);
+
+	 	if(random==1)
+                {
+                        System.out.println("EMPLOYEE IS PRESENT TODAY");
+			System.out.println("Wage/Hr=20 and Fulldayhours=8");
+			int wage=20*8;
+			System.out.println("WAGE WILL BE="+ wage);
+                }
+                else
+                {
+                        System.out.println("EMPLOYEE IS ABSENT TODAY");
+                }
+
+		
+	}
+
+
+
 }
